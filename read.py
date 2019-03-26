a760b0aa1bcea43cf9ebc73b4574350f6cc07225
Wendy Wang <wangtoro0809@gmail.com>
2019-03-26 19:25:57 +0800

    add fliter in good

--- read.py
+++ read.py
@@ -19,6 +19,15 @@ new = []
 for d in data:
 	if len(d) < 100:
 		new.append(d)
-print('一共有', len(new), '比留言長度小於100')
+print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言裡面有good')
+print(good[0])
+print(good[1])
+
